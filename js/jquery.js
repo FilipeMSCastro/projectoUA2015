4b1f8a8943dfff2f241091516f460931a5355a68
FilipeCastro <filipemscastro@gmail.com>
2015-04-01 11:11:38 +0100

    1

---
+++ js/jquery.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Bruno Rocha on 28-03-2015.
+ */
+function toExercise(){
+    var myURL = "exercicio.html";
+    $("#siteBody").load(myURL);
+}
+function toJogada(){
+    var myURL = "jogada.html";
+    $("#siteBody").load(myURL);
+}
+
+function toVideo(){
+    var myURL = "video.html";
+    $("#siteBody").load(myURL);
+}
+function toHome(){
+    var myURL = "navigator.html";
+    $("#siteBody").load(myURL);
+}
+
+function toCalendar(){
+    var myURL="calendar.html";
+    $("#calendar").load(myURL);
+}
+
+function toDragYour(){
+    var myURL="yourTeam.html";
+    $("#yourTeam").load(myURL);
+}
+function toDragMy(){
+    var myURL="myTeam.html";
+    $("#myTeam").load(myURL);
+}
+function toStates(){
+    var myURL="charisma-master/index.html";
+    $("#siteBody").load(myURL);
+}
